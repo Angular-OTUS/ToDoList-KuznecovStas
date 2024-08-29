792d299a021c86179ee372aaa571d98a935fd233
staskuznetsov <stas@kuznec.team>
2024-08-29 14:25:50 +0300

    Не залил в первый коммит файлы. FIXED

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input} from '@angular/core';
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
+import {Button} from "../../interfaces/button";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [
+    MatProgressSpinner,
+    NgIf
+  ],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+@Input({required: true}) button!: Button
+  newItemValue: any;
+  @Input() disabled!: boolean;
+
+}
