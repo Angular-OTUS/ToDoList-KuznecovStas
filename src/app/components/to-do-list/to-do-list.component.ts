88b326e17c2a91f50573ef4abb796df77e05d59e
staskuznetsov <stas@kuznec.team>
2024-09-27 16:44:23 +0300

    Домашнее задание №6
    1. в интерфейсе TodoTask исправлено поле status
    2. добавлено поле статус
    3. Добавлена фильтрация ппо статусу
    4. добавлен компонент todo-item-create
    5. добавлен Fake API на основе Json-Server и реализованы CRUD
    6. в сервис todo-store добавлен метод для получения туду по статусу
    7. Мультиязычные файлы дополнены информацией

--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -7,13 +7,13 @@ import {MatIcon} from "@angular/material/icon";
 import {ToDoItemComponent} from "../to-do-item/to-do-item.component";
 import {SpinnerComponent} from "../spiner/spinner.component";
 import {ButtonComponent} from "../button/button.component";
-import {Button} from "../../interfaces/button";
 import {TranslateModule} from "@ngx-translate/core";
 import {LanguageSelectorComponent} from "../language-selector/language-selector.component";
 import {TuiInputInline} from "@taiga-ui/kit";
 import {TodoStoreService} from "../../services/todo-store.service";
 import {Subscription} from "rxjs";
 import {ToastService} from "../../services/toast.service";
+import {TodoCreateItemComponent} from "../todo-create-item/todo-create-item.component";
 
 @Component({
   selector: 'app-to-do-list',
@@ -31,31 +31,20 @@ import {ToastService} from "../../services/toast.service";
     TranslateModule,
     LanguageSelectorComponent,
     TuiInputInline,
+    TodoCreateItemComponent,
   ],
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.scss'
 })
 export class ToDoListComponent implements OnInit, OnDestroy {
   componentTitle = 'ToDo List'
-  newTitleValue: string = ''
-  newDescriptionValue: string = ''
 
-  addButton: Button = {
-    icon: "BUTTONS.ADD",
-    title: "BUTTONS.ADD_TITLE",
-    class: {
-      color: "white",
-      background: "green",
-      height: "60px",
-      width: "100px",
-    }
-  }
 
   todoItems: TodoTask[] | undefined;
   isLoading: boolean = false
   todoDescription: string = ''
   selectedItem: number | null = null;
-  editItem?: TodoTask | null = null;
+  selectedStatus: string | null = null;
   private subscription = new Subscription();
 
   constructor(
@@ -70,18 +59,9 @@ export class ToDoListComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-
     setTimeout(() => this.isLoading = true, 500)
   }
 
-  addTask() {
-    if (!this.newTitleValue) return;
-
-    this.store.addTask(this.newTitleValue, this.newDescriptionValue)
-    this.toastService.showToast("TOASTS.TASK_ADDED")
-    this.newDescriptionValue = ''
-    this.newTitleValue = ''
-  }
 
   deleteTask(id: number) {
     if (!this.todoItems) return;
@@ -90,6 +70,15 @@ export class ToDoListComponent implements OnInit, OnDestroy {
     this.toastService.showToast("TOASTS.TASK_DELETE")
   }
 
+  filterTasks(): void {
+    if (!this.todoItems) return;
+    if (this.selectedStatus === 'null') {
+      this.todoItems = this.store.getAllTasks()
+    } else {
+      this.todoItems = this.store.getTaskByStatus(this.selectedStatus)
+    }
+
+  }
 
   updateTask(task: TodoTask) {
     if (!this.todoItems) return;
@@ -98,14 +87,13 @@ export class ToDoListComponent implements OnInit, OnDestroy {
 
   }
 
-  toggleFinish(id: number) {
+  setStatus(id: number) {
     if (!this.todoItems) return;
     const item: TodoTask | undefined = this.todoItems.find(item => item.id === id);
     if (item) {
-
-      this.store.updateTask(id, {...item, finish: !item.finish})
+      this.store.updateTask(id, {...item, status: item.status === 'InProgress' ? 'Completed' : 'InProgress'})
       // выводим если задача завершена
-      if (!item.finish) {
+      if (item.status === 'Completed') {
         this.toastService.showToast("TOASTS.TASK_FINISH")
       }
     }
--- src/app/services/todo-store.service.ts
+++ src/app/services/todo-store.service.ts
@@ -1,26 +1,32 @@
 import {Injectable} from '@angular/core';
 import {TodoTask} from "../interfaces/to-do";
 import {BehaviorSubject} from "rxjs";
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoStoreService {
-  private _todoItems: TodoTask[] = [
-    {id: 1, title: 'Проснуться', description: "утро добрым не бывает", finish: true},
-    {id: 2, title: 'Умыться', description: '', finish: false},
-    {id: 3, title: 'Поесть', description: '', finish: false},
-    {id: 7, title: 'Сходить в магазин', description: 'Список покупок:\n1. Хлеб,\n2. Макароны', finish: false},
-  ];
+  private _apiBaseURL: string = 'http://localhost:3000';
+  private _todoItems: TodoTask[] = [];
 
   private _todoItemsSubject: BehaviorSubject<TodoTask[]> = new BehaviorSubject<TodoTask[]>(this._todoItems);
   todoItems$ = this._todoItemsSubject.asObservable();
 
-  constructor() {
+  constructor(private http: HttpClient) {
+    this.getAllTasks()
   }
 
   getAllTasks() {
+    this.http.get<TodoTask[]>(this._apiBaseURL + '/todos').subscribe(data => {
+      this._todoItems = data;
+      this._todoItemsSubject.next(this._todoItems);
+    });
     return [...this._todoItemsSubject.value];
+  }
+
+  getTaskByStatus(status: string | null) {
+    return this._todoItems.filter(item => item.status === status);
   }
 
   addTask(title: string, description: string) {
@@ -28,24 +34,49 @@ export class TodoStoreService {
     const maxId: number = Math.max(...this._todoItems!.map(item => item.id));
     const nextID: number = maxId + 1
 
-    this._todoItems?.push(
-      {id: nextID, title: title, description: description, finish: false}
-    )
-    this._todoItemsSubject.next(this._todoItems);
+    // отправляем на сервер
+    this.http.post(this._apiBaseURL + '/todos', {
+      id: nextID.toString(),
+      title: title,
+      description: description,
+      status: 'InProgress'
+    }).subscribe({
+      next: () => {
+        this._todoItems?.push({id: nextID, title: title, description: description, status: 'InProgress'})
+        this._todoItemsSubject.next(this._todoItems);
+      },
+      error: (error) => {
+        console.error('todo-store-service|deleteTask', error);
+        return;
+      }
+    });
+
+
   }
 
   deleteTask(id: number) {
-    this._todoItems = this._todoItems.filter(item => item.id !== id);
-    this._todoItemsSubject.next(this._todoItems)
+    // отправляем на сервер
+    this.http.delete(this._apiBaseURL + '/todos/' + id).subscribe({
+      next: () => {
+        this._todoItems = this._todoItems.filter(item => item.id !== id);
+        this._todoItemsSubject.next(this._todoItems);
+      },
+      error: (error) => {
+        console.error('todo-store-service|deleteTask', error);
+      }
+    });
   }
 
   updateTask(id: number, item: TodoTask) {
     const index = this._todoItems.findIndex(item => item.id === id);
     if (index === -1) {
-      console.error(`Task with id ${id} not found`);
+      console.error(`todo-store-service|deleteTask - Task with id ${id} not found`);
       return;
     }
     this._todoItems[index] = item;
     this._todoItemsSubject.next(this._todoItems)
+
+    // отправляем на сервер
+    this.http.put(this._apiBaseURL + '/todos/' + id, item).subscribe();
   }
 }
